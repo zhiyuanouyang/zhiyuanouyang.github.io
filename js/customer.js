bc8202184346256f1243b2122a1141a493980a50
tiancaioyzy <tiancaioyzy@gmail.com>
2020-09-13 10:43:26 -0700

    1) hover and show wechat QR code

--- js/customer.js
+++ js/customer.js
@@ -515,6 +515,14 @@ fillDiv('databases', databases);
 fillDiv('frameworks', frameworks);
 fillDiv('tools', tools);
 
+    // popovers initialization - on hover
+    $('[data-toggle="popover-hover"]').popover({
+      html: true,
+      trigger: 'hover',
+      placement: 'right',
+      content: function () { return '<img src="' + $(this).data('img') + '" />'; }
+    });
+
 
 
 
